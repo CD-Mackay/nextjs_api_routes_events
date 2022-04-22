c2df4cd069353075edac713452fd14a43dee6eb2
Connor Mackay <connor.mackay@gmail.com>
2022-04-22 15:03:07 -0400

    Add get route for comments

--- starting-project/components/input/comment-list.js
+++ starting-project/components/input/comment-list.js
@@ -2,21 +2,22 @@ import classes from "./comment-list.module.css";
 import { useEffect } from "react";
 
 function CommentList(props) {
-  const comments = props.comments;
   console.log(props);
+  const comments = props.comments;
+
 
   return (
     <ul className={classes.comments}>
-      {/* {comments.length > 0 && comments.map((comment) => {
+      {comments && comments.map((comment) => {
         return (
-          <li key={comment.commentId}>
+          <li key={comment._id}>
             <p>{comment.text}</p>
             <div>
               by <address>{comment.name}</address>
             </div>
           </li>
         );
-      })} */}
+      })}
       {/* Render list of comments - fetched from API */}
     </ul>
   );
--- starting-project/pages/api/events/[...commentId].js
+++ starting-project/pages/api/events/[...commentId].js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import build from "next/dist/build";
 import path from "path";
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 export function buildPath() {
   return path.join(process.cwd(), "dummy-comments.json");
@@ -14,14 +14,12 @@ export function extractData(filePath) {
 }
 
 async function handler(req, res) {
-
-
   const client = await MongoClient.connect(
     "mongodb+srv://Connor:Shawt0graph@authcluster.jdoeb.mongodb.net/events?retryWrites=true&w=majority"
   );
-    console.log("connected to database");
+  console.log("connected to database");
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const eventId = req.query.commentId[0];
 
     const commentObject = {
@@ -32,24 +30,34 @@ async function handler(req, res) {
     };
 
     const db = client.db();
-    const result = await db.collection('comments').insertOne({ comment: commentObject });
+    const result = await db
+      .collection("comments")
+      .insertOne({ comment: commentObject });
     console.log(result);
+
+    newComment.id = result.insertedId;
     const filePath = buildPath();
     const commentList = extractData(filePath);
     commentList.push(commentObject);
 
-     fs.writeFileSync(filePath, JSON.stringify(commentList));
+    fs.writeFileSync(filePath, JSON.stringify(commentList));
 
-    res.status(201).json({ message: "heyyooo it worked!"});
-    
-  } else if (req.method === "GET") {
-    const filePath = buildPath();
+    res.status(201).json({ message: "heyyooo it worked!" });
+  }
+  if (req.method === "GET") {
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    const comments = fs.readFileSync(filePath);
-    res.json({ message: "Why are you here? This is an API route", comments: JSON.parse(comments) });
+      console.log(documents)
+
+    res.status(200).json({ comments: documents });
   }
 
   client.close();
-};
+}
 
 export default handler;
