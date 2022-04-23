fc939f49fc328beb8fd411239c58ff46e714e5d3
Connor Mackay <connor.mackay@gmail.com>
2022-04-23 09:45:04 -0400

    debug context

--- starting-project/store/notification-context.js
+++ starting-project/store/notification-context.js
@@ -1,14 +1,29 @@
 import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification: function() {},
+  showNotification: function(notificationData) {},
   hideNotification: function() {}
 });
 
-export function NotificationContextProvider() {
-  return <NotificationContextProvider>
+export function NotificationContextProvider(props) {
+
+  const [activeNotification, setActiveNotification] = useState();
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  };
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  };
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler
+  };
+
+  return <NotificationContextProvider value={context}>
     {props.children}
   </NotificationContextProvider>
 };
